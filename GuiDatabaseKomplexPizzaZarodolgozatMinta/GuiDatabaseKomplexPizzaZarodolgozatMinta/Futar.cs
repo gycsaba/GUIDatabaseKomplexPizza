713a3d0135606ca857b7f425ad6f319a8d49afac
Gyuris Csaba <gycsaba@vasvari.hu>
2018-12-15 18:17:02 +0100

    Futar osztály

---
+++ GuiDatabaseKomplexPizzaZarodolgozatMinta/GuiDatabaseKomplexPizzaZarodolgozatMinta/Futar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiDatabaseKomplexPizzaZarodolgozatMinta
+{
+    class Futar
+    {
+        private int azon;
+        private string nev;
+        private string tel;
+    }
+}
