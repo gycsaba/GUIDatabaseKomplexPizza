29d9c5f022b469d277a5aa934714eb83903048de
Gyuris Csaba <gycsaba@vasvari.hu>
2018-12-15 19:48:59 +0100

    Kapcsoló táblák osztályai: Tetel és Rendelelés

---
+++ GuiDatabaseKomplexPizzaZarodolgozatMinta/GuiDatabaseKomplexPizzaZarodolgozatMinta/Rendeles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiDatabaseKomplexPizzaZarodolgozatMinta
+{
+    class Rendeles
+    {
+        private int azon;
+        private int vevoAzon;
+        private int futarAzon;
+        DateTime datum;
+    }
+}
