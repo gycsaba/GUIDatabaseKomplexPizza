9d95174b4d955e348a3f9b3582dcd5ef7b9db7dd
Gyuris Csaba <gycsaba@vasvari.hu>
2019-01-05 20:35:54 +0100

    Szolgáltatás réteg és Szolgáltatás és Adattár osztálydiagram

---
+++ GuiDatabaseKomplexPizzaZarodolgozatMinta/GuiDatabaseKomplexPizzaZarodolgozatMinta/SzolgaltatasRendelesek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiDatabaseKomplexPizzaZarodolgozatMinta
+{
+    class SzolgaltatasRendelesek
+    {
+        AdattarRendelesek adattarRendelesek;
+
+        SzolgaltatasRendelesek()
+        {
+            adattarRendelesek = new AdattarRendelesek();
+        }
+    }
+}
---
+++ GuiDatabaseKomplexPizzaZarodolgozatMinta/GuiDatabaseKomplexPizzaZarodolgozatMinta/SzolgaltatasTetelek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiDatabaseKomplexPizzaZarodolgozatMinta
+{
+    class SzolgaltatasTetelek
+    {
+        AdattarTetelek adattarTetelek;
+
+        SzolgaltatasTetelek()
+        {
+            adattarTetelek = new AdattarTetelek();
+        }
+    }
+}
